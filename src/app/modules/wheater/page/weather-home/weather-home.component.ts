fa34e159f5e652cf4af97e62a3177b8ae2b0e1c9
kanes.chan@gmail.com <kanes.chan@gmail.com>
2025-01-09 10:39:24 -0300

    Implementado chamada para a API e criação da model WeatherDatas.

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: ['./weather-home.component.scss']
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
+  constructor(private weatherService: WeatherService){}
+  initialCityName = 'Curitiba';
+  weatherDatas!: WeatherDatas;
 
+  ngOnInit(): void {
+    this.getWheatherDatas(this.initialCityName);
+  }
+
+  getWheatherDatas(cityName: string): void{
+    this.weatherService.getWeatherCity(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log(this.weatherDatas)
+      },
+      error: (error) => console.log(error),
+    })
+  }
 }
--- src/app/modules/wheater/services/weather.service.ts
+++ src/app/modules/wheater/services/weather.service.ts
@@ -12,7 +12,7 @@ export class WeatherService {
 
   getWeatherCity(cityName: string): Observable<any>{
     return this.http.get(
-      ''
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
     )
   }
 }
